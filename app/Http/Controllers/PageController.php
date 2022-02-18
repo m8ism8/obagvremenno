d162322be1eb1b5b9cb308d2ffe005716b5e7f94
Rus <saylerplay@gmail.com>
2022-02-18 20:13:23 +0600

    page controller update

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,9 +9,9 @@ class PageController extends Controller
     public function history(Request $request){
         $language = $request->header('accept-language');
         $content = [
-            'title' => setting('history-'.$language.'.title'),
-            'image' => setting('history-'.$language.'.image'),
-            'text' => setting('history-'.$language.'.text')
+            'title' => setting('history.title'),
+            'image' => env('APP_URL') . '/storage/'.setting('history.image'),
+            'text' => setting('history.text')
         ];
         return response()->json([
             'content' => $content,
