8a2f5715bb67c3416a79ececac7ed026dbf723f1
faraon <farhad.begimbetov1>
2022-05-23 15:09:42 +0600

    add slug

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\NewSales;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 use App\Models\Sale;
 use App\Models\City;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class PageController extends Controller
@@ -14,6 +17,116 @@ class PageController extends Controller
     public function sales()
     {
         $news_items = NewSales::query()
+                              ->select(
+                                  'id',
+                                  'title',
+                                  'subtitle',
+                                  'badge',
+                                  'image',
+                                  'text',
+                                  'created_at'
+                              )
+                              ->orderBy('created_at', 'desc')
+                              ->get()
+        ;
+        foreach ($news_items as $news) {
+            $news->slug  = Str::slug($news->title);
+            $news->image = env('APP_URL') . '/storage/' . $news->image;
+        }
+
+        return response()->json([
+                                    'news' => $news_items,
+                                ]);
+    }
+
+    public function salesById(int $id)
+    {
+        $article = NewSales::query()
+                           ->find($id)
+        ;
+        if ($article == null) {
+            return response()->json([
+                                        'message' => 'Страница не найдена',
+                                    ], 404);
+        }
+        $article->image = env('APP_URL') . '/storage/' . $article->image;
+
+        $images = json_decode($article->images);
+        if ($images != null) {
+            $newImages = [];
+
+            foreach ($images as $image) {
+                $image       = env('APP_URL') . '/storage/' . $image;
+                $newImages[] = $image;
+            }
+            $article->images = $newImages;
+        }
+
+        $images = json_decode($article->second_images);
+        if ($images != null) {
+            $newImages = [];
+            foreach ($images as $image) {
+                $image       = env('APP_URL') . '/storage/' . $image;
+                $newImages[] = $image;
+            }
+            $article->second_images = $newImages;
+        }
+
+
+        $images = json_decode($article->third_images);
+        if ($images != null) {
+            $newImages = [];
+            foreach ($images as $image) {
+                $image       = env('APP_URL') . '/storage/' . $image;
+                $newImages[] = $image;
+            }
+            $article->third_images = $newImages;
+        }
+
+        return response()->json($article);
+    }
+
+    public function history()
+    {
+        $content = [
+            'title' => setting('history.title'),
+            'image' => env('APP_URL') . '/storage/' . setting('history.image'),
+            'text'  => setting('history.text'),
+        ];
+
+        return response()->json([
+                                    'content' => $content,
+                                ]);
+    }
+
+    public function mission()
+    {
+        $content = [
+            'title' => setting('mission.title'),
+            'image' => env('APP_URL') . '/storage/' . setting('mission.image'),
+            'text'  => setting('mission.text'),
+        ];
+
+        return response()->json([
+                                    'content' => $content,
+                                ]);
+    }
+
+    public function brandInfo()
+    {
+        $content = [
+            'title' => setting('brand-info.title'),
+            'text'  => setting('brand-info.text'),
+        ];
+
+        return response()->json([
+                                    'content' => $content,
+                                ]);
+    }
+
+    public function getNews()
+    {
+        $news_items = Sale::query()
                           ->select(
                               'id',
                               'title',
@@ -24,127 +137,35 @@ class PageController extends Controller
                               'created_at'
                           )
                           ->orderBy('created_at', 'desc')
-                          ->get();
-        foreach($news_items as $news){
-            $news->slug = Str::slug($news->title);
-            $news->image = env('APP_URL').'/storage/'.$news->image;
+                          ->get()
+        ;
+        foreach ($news_items as $news) {
+            $news->image = env('APP_URL') . '/storage/' . $news->image;
         }
+
         return response()->json([
-                                    'news' => $news_items
+                                    'news' => $news_items,
                                 ]);
-    }
-
-    public function salesById(int $id)
-    {
-        $article = NewSales::query()->find($id);
-        if ($article == null) {
-            return response()->json([
-                                        'message' => 'Страница не найдена'
-                                    ],404);
-        }
-        $article->image = env('APP_URL').'/storage/'.$article->image;
-
-        $images = json_decode($article->images);
-        if ($images != null) {
-            $newImages = [];
-
-            foreach ($images as $image) {
-                $image = env('APP_URL').'/storage/' . $image;
-                $newImages[] = $image;
-            }
-            $article->images = $newImages;
-        }
-
-        $images = json_decode($article->second_images);
-        if ($images != null) {
-            $newImages = [];
-            foreach ($images as $image) {
-                $image = env('APP_URL') . '/storage/' . $image;
-                $newImages[] = $image;
-            }
-            $article->second_images = $newImages;
-        }
-
-
-        $images = json_decode($article->third_images);
-        if ($images != null) {
-            $newImages = [];
-            foreach ($images as $image) {
-                $image = env('APP_URL') . '/storage/' . $image;
-                $newImages[] = $image;
-            }
-            $article->third_images = $newImages;
-        }
-
-        return response()->json($article);
-    }
-
-    public function history(){
-        $content = [
-            'title' => setting('history.title'),
-            'image' => env('APP_URL') . '/storage/'.setting('history.image'),
-            'text' => setting('history.text')
-        ];
-        return response()->json([
-            'content' => $content,
-        ]);
-    }
-    public function mission(){
-        $content = [
-            'title' => setting('mission.title'),
-            'image' => env('APP_URL') . '/storage/'.setting('mission.image'),
-            'text' => setting('mission.text')
-        ];
-        return response()->json([
-            'content' => $content,
-        ]);
-    }
-    public function brandInfo(){
-        $content = [
-            'title' => setting('brand-info.title'),
-            'text' => setting('brand-info.text')
-        ];
-        return response()->json([
-            'content' => $content,
-        ]);
-    }
-    public function getNews(){
-        $news_items = Sale::query()
-            ->select(
-                'id',
-                        'title',
-                        'subtitle',
-                        'badge',
-                        'image',
-                        'text',
-                        'created_at'
-            )
-            ->orderBy('created_at', 'desc')
-            ->get();
-        foreach($news_items as $news){
-            $news->image = env('APP_URL').'/storage/'.$news->image;
-        }
-        return response()->json([
-            'news' => $news_items
-        ]);
     }
 
     public function new(int $id)
     {
-        $article = Sale::query()->find($id);
+        $article = Sale::query()
+                       ->find($id)
+        ;
         if ($article == null) {
             return response()->json([
-                'message' => 'Страница не найдена'
-            ],404);
+                                        'message' => 'Страница не найдена',
+                                    ], 404);
         }
-        $article->image = env('APP_URL').'/storage/'.$article->image;
+        $article->image = env('APP_URL') . '/storage/' . $article->image;
 
         $images = json_decode($article->images);
         if ($images != null) {
             $newImages = [];
 
             foreach ($images as $image) {
-                $image = env('APP_URL').'/storage/' . $image;
+                $image       = env('APP_URL') . '/storage/' . $image;
                 $newImages[] = $image;
             }
             $article->images = $newImages;
@@ -154,7 +175,7 @@ class PageController extends Controller
         if ($images != null) {
             $newImages = [];
             foreach ($images as $image) {
-                $image = env('APP_URL') . '/storage/' . $image;
+                $image       = env('APP_URL') . '/storage/' . $image;
                 $newImages[] = $image;
             }
             $article->second_images = $newImages;
@@ -165,19 +186,47 @@ class PageController extends Controller
         if ($images != null) {
             $newImages = [];
             foreach ($images as $image) {
-                $image = env('APP_URL') . '/storage/' . $image;
+                $image       = env('APP_URL') . '/storage/' . $image;
                 $newImages[] = $image;
             }
             $article->third_images = $newImages;
         }
+        $productIds = DB::table('sale_products')
+                        ->where('sale_id', $id)
+                        ->pluck('product_id')
+                        ->toArray()
+        ;;
+        $products = Product::query()
+                           ->select(
+                               'id',
+                               'title',
+                               'price',
+                               'new_price',
+                               'image',
+                           )
+                           ->whereIn('id', $productIds)
+                           ->get()
+        ;
+        foreach ($products as $product) {
+            if (json_decode($product->image) != null) {
+                $product->image = env('APP_URL') . '/storage/' . json_decode($product->image)[0];
+            } else {
+                $product->image = env('APP_URL') . '/storage/' . $product->image;
+            }
+        }
+        $article->products = $products;
 
         return response()->json($article);
     }
 
-    public function getCities(){
-        $cities = City::with('fillials')->get();
+    public function getCities()
+    {
+        $cities = City::with('fillials')
+                      ->get()
+        ;
+
         return response()->json([
-            'cities' => $cities
-        ]);
+                                    'cities' => $cities,
+                                ]);
     }
 }
