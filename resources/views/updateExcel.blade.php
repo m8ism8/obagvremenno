a9bb105f61ad841224c5ca0c0f8841ee5e24941b
faraon <farhad.begimbetov1>
2022-05-26 15:57:08 +0600

    add slug

--- resources/views/updateExcel.blade.php
+++ resources/views/updateExcel.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <div class="container" style="margin-top: 10%">
-        <h3>Имортируйте товары из excel файла!</h3>
+        <h3>Обновляйте товары из excel файла!</h3>
         <form action="/admin/excel-import-update/send" method="post" enctype="multipart/form-data">
             @csrf
             <input type="file" name="file" style="margin-top: 15px">
