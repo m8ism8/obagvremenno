8032a8fe0566bff6b3d8bfcd1330b662a23daa04
faraon <farhad.begimbetov1>
2022-06-07 17:59:50 +0600

    add slug

--- app/Imports/UpdateExcel.php
+++ app/Imports/UpdateExcel.php
@@ -25,18 +25,20 @@ class UpdateExcel implements ToCollection
             if ($row[0] == 'Артикул' or $row[0] == null) {
 
             } else {
+
                 $check = Product::query()
                                 ->where('code', $row[0])
-                                ->exists()
                 ;
-                if (!$check) {
+                if (!$check->exists()) {
                 } else {
+                    $product     = $check->first();
                     $code        = $row[0];
-                    $price       = $row[1];
-                    $remainder   = $row[2];
-                    $title       = $row[3];
-                    $description = $row[4];
-                    $content     = $row[5];
+                    $price       = $row[1] ?? $product->price;
+                    $remainder   = $row[2] ?? $product->price;
+                    $new_price   = $row[3] ?? $product->price;
+                    $title       = $row[4] ?? $product->price;
+                    $description = $row[5] ?? $product->price;
+                    $content     = $row[6] ?? $product->price;
 
                     Product::query()
                            ->where('code', $code)
@@ -46,6 +48,7 @@ class UpdateExcel implements ToCollection
                                         'seo_title'       => $title,
                                         'seo_description' => $description,
                                         'seo_content'     => $content,
+                                        'new_price'       => $new_price,
                                     ])
                     ;
                 }
