26023ecf5126c8be0e2d0a643192b4d723da827b
faraon <farhad.begimbetov1>
2022-06-09 08:12:07 +0600

    add slug

--- resources/views/vendor/voyager/carts/read.blade.php
+++ resources/views/vendor/voyager/carts/read.blade.php
@@ -1,6 +1,5 @@
 @extends('voyager::master')
 
-{{dd($cart, $products)}}
 @section('page_header')
     @parent
     <div class="container">
@@ -8,88 +7,76 @@
         <h3>Заказ #{{ $cart->id }} оформлен {{$cart->created_at}}</h3>
         <table class="table" style="margin-top:2rem">
             <tr>
-                <th>Обслуживающие магазины</th>
-                <th> {{$shopsTitle}}</th>
+                <th>Имя</th>
+                <th> {{$cart->name}}</th>
+            <tr>
+            <tr>
+                <th>Номер телефона</th>
+                <th>{{$cart->phone}}</th>
+            <tr>
+            <tr>
+                <th>Почта</th>
+                <th>{{$cart->email}}</th>
             <tr>
             <tr>
                 <th>Адрес</th>
-                <th>Страна {{$order->country}}, Город {{$order->city}}, Индекс {{$order->index}},
-                    Адрес {{$order->address}}</th>
+                <th>@if($cart->delivery_type == 'pickup')
+                        Самовывоз
+                    @else
+                        {{$cart->address}}
+                    @endif</th>
             <tr>
             <tr>
                 <th>Общая сумма</th>
-                <th>{{$order->total_sum}}</th>
+                <th>{{$cart->price}}</th>
             <tr>
             <tr>
-                <th>Вес</th>
-                <th>{{$order->weight}}</th>
+                <th>Вид оплаты</th>
+                <th>{{$cart->payment_type}}</th>
             <tr>
             <tr>
-                <th>Цена доставки</th>
-                <th>{{$order->delivery_price}}</th>
+                <th>Бонусов списано</th>
+                <th>{{$cart->bonus_waste}}</th>
             <tr>
+            <tr>
+                <th>Бонусов начислено</th>
+                <th>{{$cart->bonuses_accrued}}</th>
+            <tr>
+
             <tr>
                 <th>Статус</th>
-                <th>{{$order->status}}</th>
+                <th>{{$cart->payment_status}}</th>
             <tr>
         </table>
     </div>
 @stop
 
-
-{{--@section('content')--}}
-{{--    <div class="container">--}}
-{{--        <div class="page-content read container-fluid">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-md-12">--}}
-{{--                    <div style="display: flex; justify-content: space-between;">--}}
-{{--                        <div>--}}
-{{--                            <p>Товар</p>--}}
-{{--                        </div>--}}
-{{--                        <div style="width: 355px;">--}}
-{{--                        </div>--}}
-{{--                        <div>--}}
-{{--                            <p>Цена за единицу</p>--}}
-{{--                        </div>--}}
-{{--                        <div>--}}
-{{--                            <p>Кол-во</p>--}}
-{{--                        </div>--}}
-{{--                        <div>--}}
-{{--                            <p>Сумма</p>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    @foreach ($products as $product)--}}
-{{--                        <div class="panel panel-bordered" style="padding-bottom:5px;">--}}
-{{--                            <div class="panel-heading"--}}
-{{--                                 style="border-bottom:0; margin-left:10px; padding:20px; display: flex; justify-content: space-between;">--}}
-{{--                                <a href="{{$product->link}}">--}}
-{{--                                    <div style="display: flex;">--}}
-{{--                                        <img src="{{ $product->image }}" alt="">--}}
-{{--                                        <div style="margin-left: 25px; color: black">--}}
-{{--                                            <h4>Товар: {{ $product->title }}</h4>--}}
-{{--                                            <p>Цвет: {{ $product->color }}</p>--}}
-{{--                                            <p>Размер: {{ $product->size }}</p>--}}
-{{--                                            <p>Комментарий: {{ $product->comment }}</p>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </a>--}}
-{{--                                <div>--}}
-{{--                                    <h4>{{$product->price}}</h4>--}}
-{{--                                </div>--}}
-{{--                                <div>--}}
-{{--                                    <h4>{{$product->quantity}}</h4>--}}
-{{--                                </div>--}}
-{{--                                <div>--}}
-{{--                                    <h4>{{$product->price * $product->quantity}}</h4>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                            </a>--}}
-{{--                        </div>--}}
-{{--                    @endforeach--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+@section('content')
+    <div class="container">
+        <div class="page-content read container-fluid">
+            <div class="row">
+                <h3>Товары</h3>
+                    <table class="table" style="margin-top:2rem">
+                        <tr>
+                            <th>Название</th>
+                            <th>Код товара</th>
+                            <th>Цена</th>
+                        <tr>
+                        @foreach ($products as $product)
+                            <tr>
+                                <th>{{$product->title}}</th>
+                                <th>{{$product->code}}</th>
+                                <th>@if($product->new_price != null)
+                                        {{$product->new_price}}
+                                    @else
+                                        {{$product->price}}
+                                    @endif</th>
+                            <tr>
+                        @endforeach
+                    </table>
+            </div>
+        </div>
+    </div>
 
     {{-- Single delete modal --}}
     <div class="modal modal-danger fade" tabindex="-1" id="delete_modal" role="dialog">
@@ -116,4 +103,4 @@
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
-@stop
+    @stop
