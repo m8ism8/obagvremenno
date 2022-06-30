e7f85c7c1dd3b515b4ddb91e6c2203f112b64dc1
faraon <farhad.begimbetov1>
2022-06-30 17:23:08 +0600

    add

--- app/Http/Resources/SaleResource.php
+++ app/Http/Resources/SaleResource.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Resources;
+
 use Illuminate\Http\Resources\Json\JsonResource;
 
 use App\Http\Resources\ProductResource;
@@ -10,23 +11,30 @@ class SaleResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         $image = $this->image;
-        if($image && str_split($image, 4)[0] != 'http' ) {
-            $image = env('APP_URL').'/storage/'.$this->image;
+        if ($image && str_split($image, 4)[0] != 'http') {
+            $image = env('APP_URL') . '/storage/' . $this->image;
         }
+
+        if ($this->preview_image != null) {
+            $this->preview_image = env('APP_URL') . '/storage/' . $this->preview_image;
+        }
+
         return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'text' => $this->text,
-            'image' => $image,
-            'is_main' => $this->is_main,
+            'id'         => $this->id,
+            'title'      => $this->title,
+            'text'       => $this->text,
+            'image'      => $image,
+            'is_main'    => $this->is_main,
             'created_at' => $this->created_at,
-            'products' => ProductResource::collection($this->products)
+            'preview_image' => $this->preview_image,
+            'products'   => ProductResource::collection($this->products),
         ];
     }
 
